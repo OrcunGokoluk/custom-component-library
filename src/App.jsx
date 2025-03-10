67a6f2dc754bf702d4177943080fe2b3ce162b8b
OrcunGokoluk <orcun.gokoluk@outlook.com>
2025-03-10 16:54:26 +0300

    add-tooltip

--- src/App.jsx
+++ src/App.jsx
@@ -16,22 +16,22 @@ function App() {
         <HorizontalShowcaseSection>
           <Badge text="Badge"/>
           <Badge color="red" text="Badge"/>
-          <Badge color="yellow"text="Badge"/>
           <Badge color="green"text="Badge"/>
           <Badge color="blue"text="Badge"/>
           <Badge color="indigo"text="Badge"/>
           <Badge color="purple"text="Badge"/>
           <Badge color="pink"text="Badge"/>
+          <Badge color="yellow"text="Badge"/>
         </HorizontalShowcaseSection>
         <HorizontalShowcaseSection>
           <Badge text="Badge" border={false}/>
           <Badge color="red" text="Badge"border={false}/>
-          <Badge color="yellow"text="Badge"border={false}/>
           <Badge color="green"text="Badge"border={false}/>
           <Badge color="blue"text="Badge"border={false}/>
           <Badge color="indigo"text="Badge"border={false}/>
           <Badge color="purple"text="Badge"border={false}/>
           <Badge color="pink"text="Badge"border={false}/>
+          <Badge color="yellow"text="Badge"border={false}/>
         </HorizontalShowcaseSection>
       </ShowcaseWrapper>
       {/* Badges */}
@@ -40,6 +40,13 @@ function App() {
       {/* Tooltip */}
       <ShowcaseWrapper>
         <Tooltip/>
+        <Tooltip color="red"/>
+        <Tooltip color="green"/>
+        <Tooltip color="blue"/>
+        <Tooltip color="indigo"/>
+        <Tooltip color="purple"/>
+        <Tooltip color="pink"/>
+        <Tooltip color="yellow"/>
       </ShowcaseWrapper>
       {/* Tooltip */}
     </main>
--- src/components/tooltip/Tooltip.jsx
+++ src/components/tooltip/Tooltip.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
+import { GrTooltip } from "react-icons/gr";
+import { IoClose } from "react-icons/io5";
 
-function Tooltip({ color="tooltip-default-color" border=true}) {
+function Tooltip({ color="tooltip-default-color",title="Tooltip", text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, at!"}) {
   return (
     <>
-      <div className="tooltip pink">
+      <div className={`tooltip ${color}`}>
+
       <div className="tooltip-body">
-          Tooltip
+        <section className={`tooltip-icon ${color}`}>
+            <GrTooltip/>
+        </section>
+        <section className="tooltip-content">
+            <h3>{title}</h3>
+            <p className={`tooltip-${color}`}>{text}</p>
+        </section>
+        <button className={`tooltip-close ${color}`}>
+            <IoClose/>
+        </button>
       </div>
-        <div className='tooltip-tail pink' >
+
+
+
+        <div className={`tooltip-tail ${color}`} >
             
         </div>
       </div>
